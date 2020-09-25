f2fa0cd650b01b896fd4604509a4f6797aa27663
Kyle Morgan <kmorgan8588@gmail.com>
2020-09-25 10:54:12 -0700

    introduces basic websockets and hanlder functions

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/kmorgan8588/go-chat-react
 
 go 1.15
+
+require github.com/gorilla/websocket v1.4.2
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+// upgrader used to set Read/Write bufffer sizes and a function to handle checking where requests come from
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+// reader takes a websocket connection and runs indefinitely until an error occurs.  It handles reading and writing messsages
+func reader(conn *websocket.Conn) {
+	for {
+
+		messageType, p, err := conn.ReadMessage()
+
+		if err != nil {
+			log.Println("Error reading message: ", err)
+			return
+		}
+
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println("Error writing message", err)
+			return
+		}
+	}
+}
+
+// serveWs handles requests to the /ws endpoint by upgrading the connection and passing off to the reader function
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println("Error upgrading connection: ", err)
+	}
+
+	reader(ws)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/ws", serveWs)
+}
 
 func main() {
-	fmt.Println("Chat App v0.01")
+	fmt.Println("Chat App v0.01 listening on port :8080")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
